f5322f58f0d4f9a5c1cff5528378531d7f748979
Eshan Pandey <pandeyeshan13@gmail.com>
2024-03-21 17:55:57 +0530

    remove movie from list button

--- netprime/core/views.py
+++ netprime/core/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User , auth
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Movie, MovieList
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 import re
@@ -13,8 +11,11 @@ import re
 @login_required(login_url='login')
 def index(request):
     movies = Movie.objects.all()
+    featured_movie = movies[len(movies)-1]
+
     context = {
         'movies': movies,
+        'featured_movie': featured_movie,
     }
     return render(request, 'index.html', context)
 
@@ -29,47 +30,43 @@ def movie(request, pk):
 
     return render(request, 'movie.html', context)
 
+
+
+
 @login_required(login_url='login')
 def my_list(request):
-     movie_list = MovieList.objects.filter(owner_user=request.user)
-     user_movie_list = []
 
-     for movie in movie_list:
+    movie_list = MovieList.objects.filter(owner_user=request.user)
+    user_movie_list = []
+
+    for movie in movie_list:
         user_movie_list.append(movie.movie)
-     
-     context = {
+
+    context = {
         'movies': user_movie_list
     }
-     return render(request, 'my_list.html', context)
+    return render(request, 'my_list.html', context)
 
-
+@login_required(login_url='login')
 def add_to_list(request):
-    
     if request.method == 'POST':
         movie_url_id = request.POST.get('movie_id')
         uuid_pattern = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
         match = re.search(uuid_pattern, movie_url_id)
-        movie_id = match.group(0) if match else None
+        movie_id = match.group() if match else None
 
-        movie = Movie.objects.get(uu_id=movie_id)
-        movie_list , created  = MovieList.objects.get_or_create( owner_user=request.user, movie=movie)
-       
-        if created :
-            response_data = {
-               'status': 'success',
-                'message': 'Added to List Successfully'
-            }
+        movie = get_object_or_404(Movie, uu_id=movie_id)
+        movie_list, created = MovieList.objects.get_or_create(owner_user=request.user, movie=movie)
+
+        if created:
+            response_data = {'status': 'success', 'message': 'Added ✓'}
         else:
-            response_data = {
-                'status': 'error',
-                'message': 'Movie Already In List'
-            }
+            response_data = {'status': 'info', 'message': 'Movie already in list'}
 
         return JsonResponse(response_data)
-
-    else :
-        return  JsonResponse({'status': 'error', 'message': 'Invalid Request'}, staus=400)
-
+    else:
+        # return error
+        return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
 
 
 def login(request):
@@ -83,11 +80,10 @@ def login(request):
             auth.login(request, user)
             return redirect('/')
         else:
-            messages.info(request, ' Username And Password Do Not Match')
+            messages.info(request, 'Credentials Invalid')
             return redirect('login')
     
     return render(request, 'login.html')
-
 
 def signup(request):
     if request.method == 'POST':
@@ -106,6 +102,8 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                # log user in
                 user_login = auth.authenticate(username=username, password=password)
                 auth.login(request, user_login)
                 return redirect('/')
@@ -115,8 +113,24 @@ def signup(request):
     else:
         return render(request, 'signup.html')
 
-
 @login_required(login_url='login')
 def logout(request):
     auth.logout(request)
-    return redirect('/')
+    return redirect('login')
+
+@login_required(login_url='login')
+def remove_from_list(request):
+    if request.method == 'POST':
+        movie_url_id = request.POST.get('movie_id')
+        uuid_pattern = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+        match = re.search(uuid_pattern, movie_url_id)
+        movie_id = match.group() if match else None
+
+        movie = get_object_or_404(Movie, uu_id=movie_id)
+        movie_list = MovieList.objects.get(owner_user=request.user, movie=movie)
+        movie_list.delete()
+
+        return JsonResponse({'status': 'success', 'message': 'Removed ✓'})
+    else:
+        # return error
+        return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
